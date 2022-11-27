195902900da262127065d9cde0cff8ed50695c8a
Mashopy <eliasgheeraert@gmail.com>
2022-11-27 23:22:37 +0100

    sm8250-common: Set GENERIC_ODM_IMAGE to true
    This flag allow QCOM media hal to set debug.stagefright.ccodec to 4 instead of 1 which match RealmeUI ccodec property

--- common.mk
+++ common.mk
@@ -269,6 +269,8 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
 
+GENERIC_ODM_IMAGE := true
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
